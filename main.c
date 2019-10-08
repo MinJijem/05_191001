afe07abc80604188ce41221eae0d828a714567df
MinJijem <55428081+MinJijem@users.noreply.github.com>
2019-10-08 15:30:46 +0900

    06_05

--- main.c
+++ main.c
@@ -53,17 +53,12 @@ int main(int argc, char *argv[]) {
 	
 	}
 	
-	//practice 04
-	
-	int i=0,meter;
-	
-	while (i<3)
-	{
-		
-		meter=i*1609;
-		printf("%d mile is %d meter.\n",i,meter);
-		i++;
-	}
 
+	
+	//practice 05
+	int i;
+	for (i=0;i<10;i++)
+		printf("Hello World!\n");
+	
 	return 0;
 }
